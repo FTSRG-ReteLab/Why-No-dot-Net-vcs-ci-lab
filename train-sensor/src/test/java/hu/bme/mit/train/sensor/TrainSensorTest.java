23c0180f97b8c893696e79dca2f324733fcc07c0
Menyus <menyus777@gmail.com>
2020-03-29 12:13:56 +0200

    Implemented test cases

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -6,15 +6,42 @@ import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
+    TrainController mockTC;
+    TrainUser mockTU;
+    TrainSensorImpl trainSensor;
 
     @Before
     public void before() {
-        // TODO Add initializations
+        mockTC = mock(TrainController.class);
+        mockTrainUser = mock(TrainUser.class);
+        trainSensor = new TrainSensorImpl(mockTC, mockTrainUser);
     }
 
     @Test
     public void ThisIsAnExampleTestStub() {
         Tachograph table = new Tachograph();
         Assert.assertFalse(table.table.isEmpty());
+    }
+
+    @Test
+    public void AbsoluteMinMargin() {
+        trainSensor.overrideSpeedLimit(-1);
+        verify(mockTrainUser, times(1)).setAlarmState(true);
+    }
+    @Test
+    public void AbsoluteMaxMargin() {
+        trainSensor.overrideSpeedLimit(501);
+        verify(mockTrainUser, times(1)).setAlarmState(true);
+    }
+    @Test
+    public void AbsoluteMaxMargin() {
+        trainSensor.overrideSpeedLimit(10);
+        rainSensor.overrideSpeedLimit(3);
+        verify(mockTrainUser, times(1)).setAlarmState(true);
+    }
+    @Test
+    public void RelativeMaxMargin() {
+        trainSensor.overrideSpeedLimit(300);
+        verify(mockTrainUser, times(0)).setAlarmState(false);
     }
 }
